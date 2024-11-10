20dcb8cbc1f1677cf8519996941bc5af699b64b6
ELTQ <139846244+ELTQ@users.noreply.github.com>
2024-11-10 13:37:48 -0700

    Update poker_visual.py

--- Project/poker_visual.py
+++ Project/poker_visual.py
@@ -1,27 +1,39 @@
 import cv2 as cv
-import pandas as pd
+from skimage.filters import sobel
 import numpy as np
-import torch
-import math
+from sklearn.neural_network import MLPClassifier
+
 
 f = open("cards.txt")
 cards = f.readlines()
 cards = [c.replace('\n', '') for c in cards]
-print(cards)
 
-def preprocess(image):
-    image = cv.cvtColor(image, cv.COLOR_BGR2GRAY)
-    return image
-img = cv.imread("Training_images\heart.png")
-img = preprocess(img)
-cv.imshow("Heart", img)
 
-poker = cv.imread("Testing_images\king_of_clubs2.png")
-poker = cv.resize(poker,(250, 350))
-canny = cv.Canny(poker,100,200)
-cropped = canny[5:75, 2:35]
 
-cv.imshow("Poker", poker)
-cv.imshow("Canny", canny)
-cv.imshow("Cropped", cropped)
-k = cv.waitKey(0)
+
+X = []
+Y = []
+
+def preprocess(X):
+    resized = cv.resize(X, (28, 28))
+    sobel_filtered = sobel(resized)
+    flattened = sobel_filtered.flatten()
+    return flattened
+
+for card in cards:
+    card = card.replace('Training_images/', 'Training_images2/')
+    img = cv.imread(card)
+    features = preprocess(img)
+    name = card.replace('Training_images2/', '').replace('.png', '')
+    X.append(features)
+    Y.append(name)
+
+
+X = np.array(X)
+Y = np.array(Y)
+
+model = MLPClassifier(hidden_layer_sizes=(128, 64, 32, 16, 8), activation='relu', max_iter=50000)
+model.fit(X, Y)
+
+import joblib
+joblib.dump(model, 'model.pkl')

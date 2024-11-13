232ba5fece9cc86d0472019167eed5e7cfaf2b7c
ELTQ <139846244+ELTQ@users.noreply.github.com>
2024-11-12 20:17:11 -0700

    Update poker_visual_presentation.py

--- Project/poker_visual_presentation.py
+++ Project/poker_visual_presentation.py
@@ -1,1 +1,19 @@
+import joblib
+from poker_visual import preprocess
+import cv2 as cv
+from poker_solver import Card
+p_model = joblib.load('model.pkl')
+
+Done = False
+while not Done:
+    img = input("Enter the path for the card: ")
+    pred = p_model.predict(preprocess(cv.imread(img)).reshape(1, -1))
+    print(pred)
+    finish = input("Are you finished? (y/n): ")
+    if finish == "y":
+        Done = True
+    elif finish == "n":
+        Done == False
+    else:
+        finish = input("Are you finished? (y/n): ")
 
